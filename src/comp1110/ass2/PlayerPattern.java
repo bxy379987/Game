132c6d3927be3b61534064300585eaf6e1e87562
BaoXiangyu <u7751023@anu.edu.au>
2023-08-19 08:19:12 +1000

    complete task 3 4 6,task 3 improved

--- src/comp1110/ass2/PlayerPattern.java
+++ src/comp1110/ass2/PlayerPattern.java
@@ -1,15 +1,43 @@
 package comp1110.ass2;
 
 public class PlayerPattern extends Pattern {
+    private boolean isplaying=false;
     private char color;//"c"表示青色，"y"表示黄色，"r"表示红色，"p"表示紫色
 
     private int dirhams; // 迪拉姆数量
     private int remainingRugs; // 剩余要放置的地毯数量
 
-    public PlayerPattern(char color, int dirhams, int remainingRugs) {
+    public PlayerPattern(String input) {
+        this.color = input.charAt(1);
+        String dirham=input.substring(2,3);
+        this.dirhams = Integer.parseInt(dirham);
+        String remainingRuy=input.substring(5,2);
+        this.remainingRugs = Integer.parseInt(remainingRuy);
+        if(input.charAt(7)=='i')isplaying=true;
+    }
+
+    public boolean isIsplaying() {
+        return isplaying;
+    }
+
+    public void setIsplaying(boolean isplaying) {
+        this.isplaying = isplaying;
+    }
+
+    public char getColor() {
+        return color;
+    }
+
+    public void setColor(char color) {
         this.color = color;
+    }
+
+    public int getDirhams() {
+        return dirhams;
+    }
+
+    public void setDirhams(int dirhams) {
         this.dirhams = dirhams;
-        this.remainingRugs = remainingRugs;
     }
 
     public int getRemainingRugs() {
--- src/comp1110/ass2/Rug.java
+++ src/comp1110/ass2/Rug.java
@@ -1,23 +1,34 @@
 package comp1110.ass2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Rug {
 
 
 
-    private boolean isCovered;
-    private char color;
+    private boolean isCovered=false;
+    private char color;//"c"表示青色，"y"表示黄色，"r"表示红色，"p"表示紫色 if empty then 'w'(white);
     private int ID;//If it is covered, what is the ID of the rug which is occupying it. And if the STATU is empty, ID=00.
     private int[] Coordination1;//And if the STATU is empty, Coordination=[0,0]
     private int[] Coordination2;
 
 
-    public Rug(boolean isCovered, char color, int ID, int[] coordination1, int[] coordination2) {
-        this.isCovered = isCovered;
-        this.color = color;
-        this.ID = ID;
-        Coordination1 = coordination1;
-        Coordination2 = coordination2;
-    }
+    public Rug(String input) {
+        String regex="^[a-z][0-9]{6}$";
+        java.util.regex.Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if(matcher.matches()==true){this.isCovered=true;}
+        this.color=input.charAt(0);
+        String IDnumberPart = input.substring(1,2);
+        this.ID=Integer.parseInt(IDnumberPart);
+        String C1numberpart=input.substring(3,2);
+        String C2numberpart=input.substring(5,2);
+        this.Coordination1[0]=C1numberpart.charAt(0);
+        this.Coordination1[1]=C1numberpart.charAt(1);
+        this.Coordination2[0]=C2numberpart.charAt(0);
+        this.Coordination2[1]=C2numberpart.charAt(1);
+    }//将一个Rug string转化为一个Rug对象
 
     public boolean isCovered() {
         return isCovered;
