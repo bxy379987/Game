fa4764c9c7b131c444e6ca03bd717a7f59f81b92
our0boros <863166709@qq.com>
2023-08-25 08:36:49 +1000

    upstream

--- src/gittest/A.java
+++ src/gittest/A.java
@@ -2,6 +2,9 @@ package gittest;
 
 public class A {
 
+    public static void main(String[] args) {
+        System.out.println("New update for Step.6");
+    }
     @Override
     public String toString(){
         return"A";
