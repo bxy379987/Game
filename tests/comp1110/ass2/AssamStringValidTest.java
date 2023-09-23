d103b1879d89dc87858e5e15ac967716d3d1b5b0
BaoXiangyu <u7751023@anu.edu.au>
2023-09-23 11:50:17 +1000

    [UPDATE] add AssamStringValidTest Xiangyu Bao

---
+++ tests/comp1110/ass2/AssamStringValidTest.java
@@ -0,0 +1,28 @@
+package comp1110.ass2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class AssamStringValidTest {
+    String assam ="A41W";
+    @Test
+    public void TestFirstCharacter(){
+        assertTrue(assam.charAt(0)=='A');
+    }
+    @Test
+    public void TestCoordinations(){
+        assertTrue(assam.charAt(1)>='0'&&assam.charAt(1)<='6');
+        assertTrue(assam.charAt(2)>='0'&&assam.charAt(2)<='6');
+    }
+    @Test
+    public void TestDirections(){
+        assertTrue(IsDirectionValid(assam.charAt(3)));
+    }
+    public static boolean IsDirectionValid(char ch){
+        return ch == 'W' || ch == 'E' || ch == 'N' || ch == 'S';
+    }
+
+
+}
