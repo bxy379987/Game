5ba5553255e7f5f0faa97a22a2bad9f75a174ac7
our0boros <863166709@qq.com>
2023-09-30 18:01:46 +1000

    [UPDATE] Game assets

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -1,8 +1,14 @@
 package comp1110.ass2.gui;
 
+import gittest.C;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.stage.Stage;
 
 public class Game extends Application {
@@ -15,7 +21,26 @@ public class Game extends Application {
     public void start(Stage stage) throws Exception {
         // FIXME Task 7 and 15
         Scene scene = new Scene(this.root, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        stage.setTitle("◀ Assam Game ▶");
+
+        startGame();
+        gameStage();
+
+
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void gameStage() {
+        Image backgroundImage = new Image("comp1110/ass2/assets/Background.png",
+                WINDOW_WIDTH, WINDOW_HEIGHT, false, false);
+        ImageView background = new ImageView(backgroundImage);
+
+        root.getChildren().add(background);
+    }
+
+    private void startGame() {
     }
 }
